148169d14173e0ef5d03703efb2383e70200b0ab
jakkoz0803 <jakubkozicki0803@gmail.com>
2025-10-17 01:17:22 +0200

    lab 2 commit - description, admin panel

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True, null=True)  # nowe pole
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
